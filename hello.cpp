f316fb1bda8c7cd36c416cc8eae81bbd61cf9770
Sri <srisowj4linux@gmail.com>
2016-04-05 08:07:35 +0530

    fit git commit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : Sri
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	add(10,20);
+	return 0;
+}
+int add(int a,int b)
+{
+	return a+b;
+}
